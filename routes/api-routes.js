65ed6ccb207221881da6c2239a113e4faea0d81d
Nicholas Vrtis <nvav91@gmail.com>
2021-03-13 15:48:43 +0100

    setting up routes and planning routes

---
+++ routes/api-routes.js
@@ -0,0 +1,12 @@
+module.exports = function (app) {
+
+// api routes 
+// POST"/api/workouts/" 
+
+// GET"/api/workouts/"
+
+// GET`/api/workouts/range`?, 
+
+// PUT"/api/workouts/" 
+
+}
---
+++ routes/html-routes.js
@@ -0,0 +1,6 @@
+
+
+
+module.exports = function (app) {
+    // 2 app.get for the stats and the exercise (res.sendfile?)
+}
